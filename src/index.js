4ddc30e936397d916a99f1ec87d232b44638c10d
JavPerDev <mojocanario7@gmail.com>
2022-04-18 00:10:51 +0100

    Implemented navbar and we are working on the swipe button to go up and down with a single button

--- src/index.js
+++ src/index.js
@@ -1,37 +1,65 @@
-/* var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    slidesPerView: 1,
-    //spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-}); */
+var menu = ['AYÚDANOS', 'ACERCA DE', 'INICIO', 'USO', 'CONTACTO'];
 
-const swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".swiper-container", {
     loop: true,
     initialSlide: 2,
-    cssMode: true,
+    allowTouchMove: false,
+    speed: 1000,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination.pagination-bottom, .swiper-pagination.pagination-top",
         clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
-const swiper2 = new Swiper(".mySwiper2", {
+
+const swiper2 = new Swiper(".swiper-container2", {
+    //loop: true,
     direction: "vertical",
     spaceBetween: 30,
+    allowTouchMove: false,
+    speed: 1400,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
     },
+    /* navigation: {
+        prevEl: ".swiper-button-prev",
+    }, */
+    navigation: {
+        nextEl: ".swiper-button-up",
+        prevEl: ".swiper-button-down",
+    }
 });
 
+const swipeUpLeft = document.querySelector(".swiper-button-up");
+
+(function () {
+    swipeUpLeft.addEventListener('click', changeProps) //swiper2.navigation.nextEl
+    //swipeUpLeft.addEventListener('keydown', changeProps)
+})();
+
+let svgLeft = document.querySelector(".svg-up-down");
+let titleLeft = document.querySelector(".infoSwipeUp")
+
+function changeProps() {
+
+    svgLeft.classList.toggle("rotate");
+    titleLeft.textContent = "Pulsa para salir";
+}
