3fee24f42ff5fb5faaeb5317af31c728a9cbe5a4
JavPerDev <mojocanario7@gmail.com>
2022-04-20 10:57:50 +0100

    Minimal changes

--- src/index.js
+++ src/index.js
@@ -39,27 +39,46 @@ const swiper2 = new Swiper(".swiper-container2", {
             return '<span class="' + className + '">' + (index + 1) + "</span>";
         },
     },
-    /* navigation: {
-        prevEl: ".swiper-button-prev",
-    }, */
     navigation: {
-        nextEl: ".swiper-button-up",
-        prevEl: ".swiper-button-down",
+        nextEl: ".swiper-button-down",
+        prevEl: ".swiper-button-up",
     }
 });
 
-const swipeUpLeft = document.querySelector(".swiper-button-up");
+
+let swipeUpLeft = document.querySelector(".swiper-button-down");
+let svgLeft = document.querySelector(".svg-up-down");
+let titleLeft = document.querySelector(".infoSwipeUp");
+
+let horizontalPag = document.querySelector("#horizontal-pag");
+let titleHeader = document.querySelector(".title");
+
 
 (function () {
     swipeUpLeft.addEventListener('click', changeProps) //swiper2.navigation.nextEl
     //swipeUpLeft.addEventListener('keydown', changeProps)
+    showTitle();
 })();
 
-let svgLeft = document.querySelector(".svg-up-down");
-let titleLeft = document.querySelector(".infoSwipeUp")
+
 
 function changeProps() {
 
+    swipeUpLeft.classList.replace(".swiper-button-down", ".swiper-button-up");
     svgLeft.classList.toggle("rotate");
     titleLeft.textContent = "Pulsa para salir";
 }
+
+/* function showTitle() {
+
+    if (horizontalPag.querySelectorAll('span.swiper-pagination-bullet-active')[0]) {
+        titleHeader.style.visi
+
+    } else if (horizontalPag.querySelectorAll('span.swiper-pagination-bullet-active')[1]) {
+
+    } else if (horizontalPag.querySelectorAll('span.swiper-pagination-bullet-active')[3]) {
+
+    } else if (horizontalPag.querySelectorAll('span.swiper-pagination-bullet-active')[4]) {
+
+    }
+} */
