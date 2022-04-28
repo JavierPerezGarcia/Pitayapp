f9627b6e03a489618c104fa93537028848669bfa
JavPerDev <mojocanario7@gmail.com>
2022-04-28 11:16:24 +0100

    Changing vite.config.js file to be able to use the project with netlify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-const path = require("path");
+/* const path = require("path");
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";
+const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/"; */
+const isGitHubPages = true;
+const path = require("path");
+const folderName = path.basename(process.cwd()) + "/";
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
 
 module.exports = {
   root: "src",
